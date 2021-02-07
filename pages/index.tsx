5823009fffcc92482c419b48ccac7aee589f32e3
lamb572 <55807998+lamb572@users.noreply.github.com>
2021-02-07 15:47:50 +0000

    mobile nav bar added needs styling and rest of the page add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import AboutCard from '../components/aboutCard'
@@ -6,6 +6,7 @@ import { NavRoutes } from '../utils/constants/navRoutes'
 import Box from '@material-ui/core/Box'
 import CaseStudyCard from '../components/caseStudyCard'
 import { bottomsUpTheme } from '../theme'
+import MobileNavBar from '../components/mobileNavBar'
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -19,15 +20,39 @@ const useStyles = makeStyles(() =>
   })
 )
 
+
+
 const Home = () => {
+
+  const [widowWidth, setWindowWidth]= useState(0)
+
   const classes = useStyles()
   function navigateToRoute(navRoutes: NavRoutes) {
     console.log(`navigate to navRoute ${navRoutes}`)
   }
 
+   React.useEffect(() => {
+     setWindowWidth(window.outerWidth)
+   })
+
+   const x =true
+
   //TODO SORT OUT STYLING
   return (
-    <div className={classes.root}>
+// 400 needs changing with what ever is chosen
+    (widowWidth < 400)
+    ?<div className={classes.root}>
+    <Box display="flex">
+      <MobileNavBar
+        onNavigateCallback={navigateToRoute}
+        activeRoute={NavRoutes.Work}
+      />
+    
+    </Box>
+  </div>
+
+
+    :<div className={classes.root}>
       <Box display="flex">
         <Box flex={1}>
           <AboutCard

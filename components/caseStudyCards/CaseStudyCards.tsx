91b1a57f1757a6452bb4a6ccd7a6de5a33b01b6d
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-02-07 21:28:10 +0000

    holder pages built out and case study cards navigating there

--- components/caseStudyCards/CaseStudyCards.tsx
+++ components/caseStudyCards/CaseStudyCards.tsx
@@ -3,55 +3,58 @@ import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
 import Image from 'next/image'
 import { bottomsUpTheme, psdsTheme } from '../../theme'
 import CaseStudyCard from './caseStudyCard/CaseStudyCard'
-import { Box } from '@material-ui/core'
-
+import { Box, Link } from '@material-ui/core'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-        flexGrow: 1
+      flexGrow: 1,
     },
   })
 )
 
 interface Props {
-    screenWidth: number
+  screenWidth: number
 }
 
 export const CaseStudyCards: React.FC<Props> = ({ screenWidth }) => {
-    const classes = useStyles()
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
-      <CaseStudyCard
-        screenWidth={screenWidth}
-        image={
-          <Image
-            src="/assets/images/bottomsUp/3_phone_mockup.png"
-            alt="3 phones displaying bottoms up card"
-            height={125}
-            width={150}
-          />
-        }
-        theme={bottomsUpTheme}
-        title="Bottoms Up!"
-        subtitle="End to end project for a Udacity course."
-      />
+      <Link href="/case-studies/bottoms-up">
+        <CaseStudyCard
+          screenWidth={screenWidth}
+          image={
+            <Image
+              src="/assets/images/bottomsUp/3_phone_mockup.png"
+              alt="3 phones displaying bottoms up card"
+              height={125}
+              width={150}
+            />
+          }
+          theme={bottomsUpTheme}
+          title="Bottoms Up!"
+          subtitle="End to end project for a Udacity course."
+        />
+      </Link>
       <Box p={1} />
-      <CaseStudyCard
-        screenWidth={screenWidth}
-        image={
-          <Image
-            src="/assets/images/psds/case_study.png"
-            alt="3 phones displaying bottoms up card"
-            height={155}
-            width={155}
-          />
-        }
-        theme={psdsTheme}
-        title="Plain Sailing Driving School"
-        subtitle="A start up company looking to go digital."
-      />
+      <Link href="/case-studies/plain-sailing-driving-school">
+        <CaseStudyCard
+          screenWidth={screenWidth}
+          image={
+            <Image
+              src="/assets/images/psds/case_study.png"
+              alt="3 phones displaying bottoms up card"
+              height={155}
+              width={155}
+            />
+          }
+          theme={psdsTheme}
+          title="Plain Sailing Driving School"
+          subtitle="A start up company looking to go digital."
+        />
+      </Link>
     </div>
   )
 }

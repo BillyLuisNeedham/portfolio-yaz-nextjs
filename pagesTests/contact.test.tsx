4ce3e3f4fa45aeb9717e3d9d3075212bccd07a2d
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-02-20 00:25:04 +0000

    test build out

--- pagesTests/contact.test.tsx
+++ pagesTests/contact.test.tsx
@@ -1,29 +1,42 @@
 import React, { ReactElement } from 'react'
-import { render, queryByAttribute, queryByText, RenderResult } from '@testing-library/react'
+import {
+  render,
+  queryByAttribute,
+  queryByText,
+  RenderResult,
+  MatcherOptions,
+} from '@testing-library/react'
 import Contact from '../pages/contact'
 import { contactStrings } from '../public/assets/strings/contact-strings'
 
 const getById = queryByAttribute.bind(null, 'id')
 
-
-
-
 describe('Contact', () => {
-    let dom: RenderResult
-    beforeEach(() => {
-        dom = render(<Contact />)
+  let dom: RenderResult
+  let nameInput: MatcherOptions
+  let emailInput: MatcherOptions
+  let messageInput: MatcherOptions
 
-    })
-
+  beforeEach(() => {
+    dom = render(<Contact />)
+    nameInput = getById(dom.container, 'name')
+    emailInput = getById(dom.container, 'email')
+    messageInput = getById(dom.container, 'message')
+  })
 
   it('renders', () => {
     const root = getById(dom.container, 'contact-root')
     expect(root).toBeInTheDocument()
-  }),
-    it('displays name error text if name is not filled out when submit button is clicked', () => {
-      const nameError = dom.queryByText(contactStrings.nameError)
+  })
+  it('renders a name input', () => {
+    expect(nameInput).toBeInTheDocument()
+  })
+  it('renders an email input', () => {
+    expect(emailInput).toBeInTheDocument()
+  })
+  it('displays name error text if name is not filled out when submit button is clicked', () => {
+    const nameError = dom.queryByText(contactStrings.nameError)
 
-
-      expect(nameError).not.toBeInTheDocument()
-    })
+    expect(nameError).not.toBeInTheDocument()
+  })
 })

de9b5933e573fd93a3945e2cee9fe1c62ff5b742
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-03-02 08:25:29 +0000

    abstracted logic

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase'
 
 // TODO abstract into .env 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBOoT8Tht5LGhk5V39n4EZ2KI-l8TILRc4',
-  authDomain: 'portfolio-yazmin.firebaseapp.com',
-  projectId: 'portfolio-yazmin',
-  storageBucket: 'portfolio-yazmin.appspot.com',
-  messagingSenderId: '1077395798987',
-  appId: '1:1077395798987:web:174e093f6912c90a88144e',
-  measurementId: 'G-9J3EC5K5E5',
-}
+
 
 try {
   firebase.initializeApp(firebaseConfig)

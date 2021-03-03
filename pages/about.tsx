7e194b2d56a150d1f047a608c2d71620aae7cf13
lamb572 <55807998+lamb572@users.noreply.github.com>
2021-03-03 18:02:44 +0000

    divs and items added, assets need updating and layout fixed

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,100 @@
 import React from "react"
-import Image from 'next/image'
+import Image from 'next/image';
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, createStyles, Theme, ThemeProvider } from '@material-ui/core/styles';
+import {theme} from '../theme/index';
+import {BODY_TEXT } from '../public/assets/strings/about-me-data';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      
+    },
+    img:{
+        borderRadius:"30px",
+
+    },
+  })
+)
 
 interface Props {
     
 }
 
-const About: React.FC = () => {
+const About: React.FC<Props> = () => {
+
+    const classes = useStyles()
+
     return (
-        <div>
-            <h1>About</h1>
-            <Image
-            src="/assets/images/Group 68@2x.png"
-            width={300}
-            height={50}
-            layout="intrinsic"
-          />
-        </div>
+        <ThemeProvider theme={theme}>
+            <div>     
+                <Typography color="secondary" variant="h3">
+                    Thanks for stopping by.
+                </Typography>
+            </div>
+            <div>
+                <Image
+                src="/assets/images/Group 68@2x.png"
+                width={300}
+                height={50}
+                layout="intrinsic"
+                />
+            </div>
+            <div>     
+                <Typography color="primary" variant="body1">
+                    {BODY_TEXT[0]}
+                </Typography>
+            </div>
+            <div>
+                <Image
+                src="/assets/images/Group 68@2x.png"
+                width={300}
+                height={50}
+                layout="intrinsic"
+                />
+            </div>
+            <div>     
+                <Typography color="primary" variant="body1">
+                    {BODY_TEXT[1]}
+                </Typography>
+            </div>
+            <div>
+                <div className={classes.img}>
+                    <Image
+                    src="/assets/images/Group 68@2x.png"
+                    width={300}
+                    height={50}
+                    layout="intrinsic"
+                    />
+                </div>
+                <div className={classes.img}>
+                    <Image
+                    src="/assets/images/Group 68@2x.png"
+                    width={300}
+                    height={50}
+                    layout="intrinsic"
+                    />
+                </div>
+                <div className={classes.img}>
+                    <Image
+                    src="/assets/images/Group 68@2x.png"
+                    width={300}
+                    height={50}
+                    layout="intrinsic"
+                    />
+                </div>
+                <div className={classes.img}>
+                    <Image
+                    src="/assets/images/Group 68@2x.png"
+                    width={300}
+                    height={50}
+                    layout="intrinsic"
+                    />
+                </div>
+            </div>
+                
+            
+        </ThemeProvider>
     )
 }
 

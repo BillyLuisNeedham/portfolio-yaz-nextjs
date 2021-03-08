8b62523d1b4a233c1dc6e0953acbf20aad8f8289
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-03-08 22:29:21 +0000

    firebase build out

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,7 +1,15 @@
 import firebase from 'firebase'
 
-// TODO abstract into .env 
 
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+}
 
 try {
   firebase.initializeApp(firebaseConfig)
--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  firebaseConfig = {
-  apiKey: process.env.firebaseConfig.apiKey,
-  authDomain: process.env.firebaseConfig.authDomain,
-  projectId: process.env.firebaseConfig.projectId,
-  storageBucket: process.env.firebaseConfig.storageBucket,
-  messagingSenderId: process.env.firebaseConfig.messagingSenderId,
-  appId: process.env.firebaseConfig.appId,
-  measurementId: process.env.firebaseConfig.measurementId,
-},
+//   firebaseConfig = {
+  // apiKey: process.env.apiKey,
+//   authDomain: process.env.firebaseConfig.authDomain,
+//   projectId: process.env.firebaseConfig.projectId,
+//   storageBucket: process.env.firebaseConfig.storageBucket,
+//   messagingSenderId: process.env.firebaseConfig.messagingSenderId,
+//   appId: process.env.firebaseConfig.appId,
+//   measurementId: process.env.firebaseConfig.measurementId,
+// },
   poweredByHeader: false,
 }
